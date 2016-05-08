93e338a36f5662bc2412114a6f2128d80993324b
Ajda Delic <ajda.delic@gmail.com>
2016-05-08 11:20:32 +0000

    Zaključek dela na II. nalogi

--- eracun.js
+++ eracun.js
@@ -47,6 +47,9 @@ function davcnaStopnja(izvajalec, zanr) {
 
 // Prikaz seznama pesmi na strani
 streznik.get('/', function(zahteva, odgovor) {
+  if(!zahteva.session.stranka){
+    odgovor.redirect('/prijava');
+  }else{
   pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
           Artist.Name AS izvajalec, Track.UnitPrice * " +
           razmerje_usd_eur + " AS cena, \
@@ -67,7 +70,8 @@ streznik.get('/', function(zahteva, odgovor) {
           vrstice[i].stopnja = davcnaStopnja(vrstice[i].izvajalec, vrstice[i].zanr);
         odgovor.render('seznam', {seznamPesmi: vrstice});
       }
-  })
+  });
+  }
 })
 
 // Dodajanje oz. brisanje pesmi iz košarice
@@ -233,12 +237,16 @@ streznik.post('/stranka', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
+    if(!zahteva.session.stranka){
+      zahteva.session.stranka=polja.seznamStrank;
+    }
     odgovor.redirect('/')
   });
 })
 
 // Odjava stranke
 streznik.post('/odjava', function(zahteva, odgovor) {
+      zahteva.session.destroy();
     odgovor.redirect('/prijava') 
 })
 
